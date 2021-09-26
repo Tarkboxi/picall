2562ba9fb0f5f99988f7948f894065623febe6f7
Manoj <manojkarthi68@gmail.com>
2021-09-26 02:54:02 -0300

    added the notification components

---
+++ src/app/components/notify-user/notify-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notify-user',
+  templateUrl: './notify-user.component.html',
+  styleUrls: ['./notify-user.component.scss']
+})
+export class NotifyUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
