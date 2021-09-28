1fca801b9ab4fc9d75ad03d1acfe292e9d96040c
Manoj <manojkarthi68@gmail.com>
2021-09-28 18:37:56 -0300

    spinner  moved to error interceptor

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -4,11 +4,12 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpErrorResponse
+  HttpErrorResponse,
+  HttpResponse
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { NotificationService } from '../services/notification/notification.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -16,7 +17,14 @@ export class ErrorInterceptor implements HttpInterceptor {
   constructor(private notificationService: NotificationService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.notificationService.setLoading(true);
     return next.handle(request).pipe(
+      map<HttpEvent<any>, any>((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          this.notificationService.setLoading(false);
+        }
+        return event;
+      }),
       catchError((error: HttpErrorResponse) => {
         this.notificationService.setLoading(false);
         return throwError(error);
